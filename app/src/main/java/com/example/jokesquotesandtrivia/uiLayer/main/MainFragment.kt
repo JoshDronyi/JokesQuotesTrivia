2dc5c0e0414c03f4642e94cf44b9c93a18420388
Josh Dronyi <joshdronyi@Raves-MacBook-Air.local>
2023-05-31 13:00:51 -0400

    Basic navigation implemented. Figuring out why module installation fails.

--- app/src/main/java/com/example/jokesquotesandtrivia/uiLayer/main/MainFragment.kt
+++ app/src/main/java/com/example/jokesquotesandtrivia/uiLayer/main/MainFragment.kt
@@ -15,24 +15,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.jokesquotesandtrivia.uiLayer.theme.JokesQuotesAndTriviaTheme
 
 
 class MainFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        val navController = findNavController()
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 JokesQuotesAndTriviaTheme {
                     HomeScreen(
-                        onJokeRequest = {},
-                        onQuoteRequest = { },
-                        onTriviaRequest = { }
+                        onJokeRequest = {
+                            val action = MainFragmentDirections.actionMainFragmentToJokeGraph()
+                            navController.navigate(action)
+                        },
+                        onQuoteRequest = {
+                            val action = MainFragmentDirections.actionMainFragmentToQuoteGraph()
+                            navController.navigate(action)
+                        },
+                        onTriviaRequest = {
+                            val action = MainFragmentDirections.actionMainFragmentToTriviaGraph()
+                            navController.navigate(action)
+                        }
                     )
                 }
             }
